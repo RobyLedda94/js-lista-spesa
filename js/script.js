caba1625cc00344a655004bbb5e80a600d773e42
RobyLedda94 <robyledda01@yahoo.it>
2024-07-02 15:17:17 +0200

    [UPD] recuopero l'elemento dal dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 // ARRAY CON LA LISTA DEGLI INGREDIENTI
 
 const ingredienti = ['pomodori', 'peperoni', 'melanzane', 'pane', 'uova'];
+
+// RECUPERO L'ELEMENTO LISTA SPESA DAL DOM
+
+const lista_spesa = document.getElementById('lista-spesa');

cc18aca78523f15a639291879590a67512d9c7cf
RobyLedda94 <robyledda01@yahoo.it>
2024-07-02 15:41:57 +0200

    [UPD] creata variebile per inderire gli ingredienti nella lista

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ const lista_spesa = document.getElementById('lista-spesa');
 // CREO LA VARABILE PER SCORRERE GLI INGREDIENTI
 
 let i = 0;
+
+// CREO IL CICLO WHILE
+
+while (i < ingredienti.length) {
+    // CREO UN ELEMENTO LI PER OGNI INGREDIENTE
+    const elementoLista = document.createElement('li');
+
+}
+

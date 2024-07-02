dcd2ebf76b9d6a95b4c0a251d8ca62fbc75c16eb
RobyLedda94 <robyledda01@yahoo.it>
2024-07-02 15:29:19 +0200

    [UPD] variabile per scorrere gli elementi dell'array

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ const ingredienti = ['pomodori', 'peperoni', 'melanzane', 'pane', 'uova'];
 // RECUPERO L'ELEMENTO LISTA SPESA DAL DOM
 
 const lista_spesa = document.getElementById('lista-spesa');
+
+// CREO LA VARABILE PER SCORRERE GLI INGREDIENTI
+
+let i = 0;
